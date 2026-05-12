1eec9055f951a784f2d6ab3b9a72eb012ca00bdd
daishi <daishi@axlight.com>
2026-05-13 00:12:51 +0900

    v1-beta.0

--- waku.config.ts
+++ waku.config.ts
@@ -1,16 +1,14 @@
+import babel from '@rolldown/plugin-babel';
 import tailwindcss from '@tailwindcss/vite';
-import react from '@vitejs/plugin-react';
+import react, { reactCompilerPreset } from '@vitejs/plugin-react';
 import { defineConfig } from 'waku/config';
 
 export default defineConfig({
   vite: {
     plugins: [
       tailwindcss(),
-      react({
-        babel: {
-          plugins: ['babel-plugin-react-compiler'],
-        },
-      }),
+      react(),
+      babel({ presets: [reactCompilerPreset()] }),
     ],
   },
 });
